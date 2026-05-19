57abdb3d377d7ffbe97defe6e10ea5ff2c411e29
Sharath Teja <sharathteja19@gmail.com>
2026-05-19 23:30:44 +0530

    fix UserModel casing

--- MiddleWare/validationMiddleware.js
+++ MiddleWare/validationMiddleware.js
@@ -3,7 +3,7 @@ import { BadRequestError, NotFoundError } from "../errors/customError.js";
 import { JOB_STATUS, JOB_TYPE } from "../utils/constants.js";
 import mongoose from "mongoose";
 import Job from "../models/JobModel.js";
-import User from "../models/temp.js";
+import User from "../models/UserModel.js";
 
 const withValidationErrors = (validateValues) => {
   return [
